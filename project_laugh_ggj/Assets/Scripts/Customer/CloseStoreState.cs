bb7ff2fc6bc685710cb33a3b28da967b8750c1be
Enes <enescubuk10@gmail.com>
2024-01-28 16:07:07 +0300

    last implements

--- project_laugh_ggj/Assets/Scripts/Customer/CloseStoreState.cs
+++ project_laugh_ggj/Assets/Scripts/Customer/CloseStoreState.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class CloseStoreState : IStateCommand
 {
     public GameObject StorePage;
     public InventorySystem InventorySystem;
+    public GameObject OptionButton;
+    private Vector2 originalPosition;
+    public RectTransform MarketMarketPosition;
     public override void Enter()
     {
+        originalPosition = OptionButton.GetComponent<RectTransform>().anchoredPosition;
+        OptionButton.GetComponent<RectTransform>().anchoredPosition = MarketMarketPosition.anchoredPosition;
         Debug.Log("CloseStoreState");
         StorePage.SetActive(true);
+
+        
     }
 
     public override void Tick()
@@ -24,5 +32,6 @@ public class CloseStoreState : IStateCommand
         StorePage.SetActive(false);
         InventorySystem.ActiveItem();
         CustomerStateMachine.Instance.ChangeState<CustomerGenerateState>();
+        OptionButton.GetComponent<RectTransform>().anchoredPosition = originalPosition;
     }
 }
